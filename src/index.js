6996ee869c47aabeca10e6ecac5df61e119dd972
leonard <40259905+leonard246@users.noreply.github.com>
2019-07-04 22:57:39 +1000

    quotes-list

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ import './styles.css';
 class App extends React.Component {
     render() {
         return (
-            <div>
-                hello world
+            <div id="container">
+                <div id="quote-box">
+                    <div id="text">The Quote</div>
+                    <div id="author">The Author</div>
+                    <button id="new-quote">Button</button>
+                    <button><a id="tweet-quote">Tweet Quote</a></button>
+                </div>
             </div>
         )
     }
