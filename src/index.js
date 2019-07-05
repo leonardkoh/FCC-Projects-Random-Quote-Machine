3ca8a4847dbb80791acdffb29f4fde74378e026a
leonard <40259905+leonard246@users.noreply.github.com>
2019-07-05 13:24:56 +1000

    functions added

--- src/index.js
+++ src/index.js
@@ -4,17 +4,39 @@ import './styles.css';
 import { AUTHORS, QUOTES } from './data.js';
 
 class App extends React.Component {    
-    getRandomNumber = Math.floor((Math.random() * 103) + 1); 
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: '',
+            author: ''
+        };
+
+        this.getQuote = this.getQuote.bind(this);
+    }
+    // random number between 0 and 103
+    getRandomNumber = () => { return  Math.floor((Math.random() * 103) + 1); } 
+
+    getQuote = () => {
+        let randomNumber = this.getRandomNumber();
+        // this.setState({
+        //     quote: QUOTES[randomNumber],
+        //     author: AUTHORS[randomNumber]
+        // });
+        this.setState({
+            quote: QUOTES[7],
+        });
+    }
 
     render() {
-        console.log(getRandomNumber);
+        console.log(QUOTES[this.getRandomNumber()]);
         return (
             <div id="container">
                 <div id="quote-box">
-                    <div id="text">The Quote</div>
+                    <div id="text">{QUOTES[this.getRandomNumber()]}</div>
                     <div id="author">The Author</div>
                     <button id="new-quote">Button</button>
                     <button><a id="tweet-quote">Tweet Quote</a></button>
+                    <input value={this.state.quote}></input>
                 </div>
             </div>
         )
