ed852d5b14002c0fc4b784e2908c890c970c50ee
leonard <40259905+leonard246@users.noreply.github.com>
2019-07-07 08:32:48 +1000

    button styles changed

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ class App extends React.Component {
         super(props);
         this.state = {
             quote: QUOTES[1],
-            author: AUTHORS[1],
+            author: '-'+AUTHORS[1],
         };
 
         this.getQuote = this.getQuote.bind(this);
@@ -19,28 +19,29 @@ class App extends React.Component {
         let randomNumber = this.getRandomNumber();
         this.setState({
             quote: QUOTES[randomNumber],
-            author: AUTHORS[randomNumber],
+            author: '-'+AUTHORS[randomNumber],
         });
     }
 
     // random number between 0 and 103
     getRandomNumber = () => { return  Math.floor((Math.random() * 103) + 1); }
-    // random color
-    // getRandomColor = () => { return '#'+Math.random().toString(16).substr(-6); }
     
     render() {
-        let randomColor = '#'+Math.random().toString(16).substr(-6);
-        // let randomBgColor = {background: randomColor}; 
-        let randomTextColor = {color: randomColor}; 
+        const randomColor = '#'+Math.random().toString(16).substr(-6);
+        const randomBgColor = {background: randomColor}; 
+        const randomTextColor = {color: randomColor};
+        const btnBorder = {color: randomColor, border: '2px solid', borderColor: randomColor, '&:hover': {display: 'none'}} 
         
         return (
-            <div id="container" class="container-fluid" style={{background: 'black'}}>
-                <div id="quote-box">
+            <div id="container" class="container-fluid" style={randomBgColor}>
+                <div id="spacer"></div>
+                <div id="quote-box" style={{background: 'white'}}>
                     <div id="text" style={randomTextColor}><h4>{this.state.quote}</h4></div>
+                    <br />
                     <div id="author" style={randomTextColor}>{this.state.author}</div>
                     <br />
-                    <button type="button" class="btn btn-outline-primary" id="new-quote" onClick={this.getQuote}>New Quote</button>
-                    <a id="tweet-quote" class="btn btn-outline-secondary" href="http://www.twitter.com/intent/tweet">Tweet Quote</a>
+                    <button type="button" style={btnBorder} class="btn" id="new-quote" onClick={this.getQuote}>New Quote</button>
+                    <a id="tweet-quote" style={btnBorder} class="btn" href="http://www.twitter.com/intent/tweet">Tweet</a>
                 </div>
             </div>
         );
